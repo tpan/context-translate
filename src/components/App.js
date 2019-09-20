4e38de99872a885c346e51e89ed2f0afff5e5fef
Thomas Pan <thomastpan@gmail.com>
2019-09-20 14:01:51 -0700

    just a regular ole react app with normal state

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import UserCreate from "./UserCreate";
+
+class App extends React.Component {
+  state = { language: "english" };
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className='ui container'>
+        <div>
+          Select a language:
+          <i
+            className='flag ca'
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className='flag nl'
+            onClick={() => this.onLanguageChange("dutch")}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
+
+export default App;
