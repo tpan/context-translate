c9c75ece2fdd3b4405a9169e2a465cd1d77cbe6f
Thomas Pan <thomastpan@gmail.com>
2019-09-23 16:39:40 -0700

    redux type flow using context

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import UserCreate from "./UserCreate";
-import LanguageContext from "../contexts/LanguageContext";
+import { LanguageStore } from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
+import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -12,22 +13,12 @@ class App extends React.Component {
   render() {
     return (
       <div className='ui container'>
-        <div>
-          Select a language:
-          <i
-            className='flag ca'
-            onClick={() => this.onLanguageChange("english")}
-          />
-          <i
-            className='flag nl'
-            onClick={() => this.onLanguageChange("dutch")}
-          />
-        </div>
-        <ColorContext.Provider value='red'>
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value='red'>
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
